09a829c2e9baee4301e59d9027cdaa0168b27686
trevorbishopdev <143108122+trevorbishopdev@users.noreply.github.com>
2023-11-21 09:45:37 -0600

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -6,3 +7,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3')
 streamlit.text('Kale Smoothie')
 streamlit.text('Hard Boiled Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
