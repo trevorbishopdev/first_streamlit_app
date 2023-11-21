85aaf47c5c8e6d6b1f0db331d7fe46ee0eda8678
trevorbishopdev <143108122+trevorbishopdev@users.noreply.github.com>
2023-11-21 09:42:56 -0600

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale Smoothie')
+streamlit.text('Hard Boiled Egg')
